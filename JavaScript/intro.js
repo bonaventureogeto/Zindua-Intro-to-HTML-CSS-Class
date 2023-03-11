3762a1b7a11cb91d7fdd0bbc6467d326e30d9f8f
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-03-11 12:45:53 +0300

    updates

---
+++ JavaScript/intro.js
@@ -0,0 +1,14 @@
+// console.log("This is my first JavaScript program!") //first program
+
+// console.log("12345 + 12345") // string
+// console.log(12345.7867 + 12345.9) // number
+
+/* 
+first comment
+second  comment
+third comment
+*/
+
+let car = "Toyota" // car is a variable name and "Toyota" is a value
+
+console.log(car)
